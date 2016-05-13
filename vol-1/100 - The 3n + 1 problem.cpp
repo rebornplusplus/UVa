d20373d50f2e85dc093189dda60f4af50343b543
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2016-05-14 01:37:26 +0600

    Create 100 - The 3n + 1 problem.cpp

---
+++ vol-1/100 - The 3n + 1 problem.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int a, b, n, i, cnt, max;
+  int tmp_a, tmp_b;
+
+  while(cin >> a >> b) {
+    tmp_a = a;
+    tmp_b = b;
+
+    if(a>b) swap(a, b);
+
+    max = 0;
+    while(a<=b) {
+      n=a++;
+      cnt=0;
+
+      while(n!=1) {
+        if(n%2) {
+          n = 3*n+1;
+          n = n >> 1;
+          cnt += 2;
+        }
+        else {
+          n = n >> 1;
+          cnt++;
+        }
+      }
+      cnt++;
+
+      if(max < cnt) max = cnt;
+    }
+
+    cout << tmp_a << " " << tmp_b << " " << max << endl;
+  }
+
+  return 0;
+}
